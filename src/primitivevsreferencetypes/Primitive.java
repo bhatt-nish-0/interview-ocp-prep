4694271b9e76719d49c48a44978ae71fc1886dfc
bhatt-nish-0 <bhattnish0@gmail.com>
2025-01-30 11:22:04 +0800

    test

---
+++ src/primitivevsreferencetypes/Primitive.java
@@ -0,0 +1,19 @@
+package primitivevsreferencetypes;
+
+public class Primitive {
+    public static void main(String[] args) {
+        boolean a=  true;
+        byte b = -128;
+        short s = -32768;
+        int i = -2147483648;
+        long l = -1000L;
+        float f = -2.87654F;
+        double d = -2.87654;
+        char c = 'c';
+
+        String x = "nischal";
+        System.out.println(x.length());
+
+        //String is an object, not a primitve type
+    }
+}
