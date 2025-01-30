b587a5ebe1a5422bc4f5761014b54d823364481b
bhatt-nish-0 <bhattnish0@gmail.com>
2025-01-30 11:53:14 +0800

    test

---
+++ src/escapechars/Escape.java
@@ -0,0 +1,8 @@
+package escapechars;
+
+public class Escape {
+    public static void main(String[] args) {
+        String r = "hello, my name is \"Andy\", what's yours?";
+        System.out.println(r);
+    }
+}
