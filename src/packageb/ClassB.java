90a9e80b0450c5672eda611f614d200da50a92cd
bhatt-nish-0 <bhattnish0@gmail.com>
2025-01-30 10:59:18 +0800

    test

--- src/packageb/ClassB.java
+++ src/packageb/ClassB.java
@@ -7,4 +7,8 @@ public class ClassB {
     }
 }
 
+//javac -d aramdayak packageb\ClassB.java
+//java -cp aramdayak packageb.ClassB
+//in the above java now knows where to find the classes!
+//in the above, the compiled classes will be placed in aramdayak folder
 // javac packageb/ClassB.java run from src folder
