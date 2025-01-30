ebbfc1fe3cee8ad8c5ce930dcaf071f94cd1f93b
bhatt-nish-0 <bhattnish0@gmail.com>
2025-01-30 12:02:33 +0800

    test

--- src/escapechars/Escape.java
+++ src/escapechars/Escape.java
@@ -4,5 +4,10 @@ public class Escape {
     public static void main(String[] args) {
         String r = "hello, my name is \"Andy\", what's yours?";
         System.out.println(r);
+
+        String r1 = "ac";
+        System.out.println(r1);
+
+
     }
 }
